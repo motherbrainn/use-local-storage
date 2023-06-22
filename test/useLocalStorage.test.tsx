084f12dae4c8a3cbb1644e749bd0e69e52baf542
Aaron Ehresman <aaron.ehr@gmail.com>
2023-06-22 13:56:07 -0700

    initial commit

--- test/useLocalStorage.test.tsx
+++ test/useLocalStorage.test.tsx
@@ -24,6 +24,7 @@ describe("useLocalStorage", () => {
     //verify the actual value from localStorage and the return from the hook
     expect(actual).toBe(expected);
     expect(actual).toBe(valueFromHook);
+    console.log("zz");
     expect(exception).toBe(undefined);
 
     //update local storage entry using hook
