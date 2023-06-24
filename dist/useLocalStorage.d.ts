71ca18d98655fa369bddd1d0869dde5775fb07df
Aaron Ehresman <aaron.ehr@gmail.com>
2023-06-23 19:35:11 -0700

    fix typescript

---
+++ dist/useLocalStorage.d.ts
@@ -0,0 +1,16 @@
+import { Dispatch, SetStateAction } from "react";
+type SetValue<T> = Dispatch<SetStateAction<T>>;
+/**
+ * useLocalStorage
+ *
+ * A lightweight hook that makes storing and modifying data in local storage as easy as setting state.
+ *
+ * @param key key for local storage instance
+ * @param initialValue initial value for local storage instance
+ * @returns current value, setter function, and exception object
+ */
+declare const useLocalStorage: <T>(key: string, initialValue: T) => [T, SetValue<T>, {
+    error: string;
+}];
+export default useLocalStorage;
+//# sourceMappingURL=useLocalStorage.d.ts.map
