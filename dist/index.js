74143a870edac20a5da2193545af81359e0c542e
Aaron Ehresman <aaron.ehr@gmail.com>
2023-06-23 23:41:42 -0700

    update complier config

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useLocalStorage = void 0;
+const useLocalStorage_1 = __importDefault(require("./src/useLocalStorage"));
+exports.useLocalStorage = useLocalStorage_1.default;
