8780945919af2d9ae2b68ec7c49212a1747e7688
Aaron Ehresman <aaron.ehr@gmail.com>
2023-06-23 21:49:36 -0700

    update compiler settings

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useLocalStorage = void 0;
-const useLocalStorage_1 = require("./src/useLocalStorage");
-exports.useLocalStorage = useLocalStorage_1.default;
